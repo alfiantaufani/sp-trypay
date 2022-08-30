841fac20506f689e8dd285216ffa429ec6541bb1
alfiantaufani <alfiantaufani@gmail.com>
2022-08-31 00:50:36 +0700

    edut

--- web/application/controllers/api/PembayaranController.php
+++ web/application/controllers/api/PembayaranController.php
@@ -52,7 +52,7 @@ class PembayaranController extends CI_Controller
             ],
             'callback_url'      => 'https://tripay.desakedungotok.com/web/api/pembayaran/callback',
             // 'return_url'        => 'https://tripay.desakedungotok.com/web/api/pembayaran/redirect',
-            'return_url'        => 'app.views.main.router.navigate("/riwayat/")',
+            'return_url'        => '/',
             'expired_time'      => (time() + (24 * 60 * 60)), // 24 jam
             'signature'         => $init->createSignature()
         ]);

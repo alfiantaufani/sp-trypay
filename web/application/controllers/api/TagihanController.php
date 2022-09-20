4326116c790e93c813b794ba9937edb14a1bd505
alfiantaufani <alfiantaufani@gmail.com>
2022-09-21 01:21:24 +0700

    get

--- web/application/controllers/api/TagihanController.php
+++ web/application/controllers/api/TagihanController.php
@@ -30,7 +30,7 @@ class TagihanController extends CI_Controller
         $data_tagihan = $tagihan->result();
 
         foreach ($data_tagihan as $value) {
-            $data_pembayaran = $this->db->get_where('pembayaran', ['id_registrasi' => $nim])->join('detail_transaksi', 'pembayaran.id=detail_transaksi.id_pembayaran')->result();
+            $data_pembayaran = $this->db->get('pembayaran', ['id_registrasi' => $nim])->join('detail_transaksi', 'pembayaran.id=detail_transaksi.id_pembayaran')->result();
             // foreach ($data_pembayaran as $detail) {
             //     $detail_pembayaran = $this->db->get_where('detail_transaksi', ['id_pembayaran', $detail->id])->row();
 

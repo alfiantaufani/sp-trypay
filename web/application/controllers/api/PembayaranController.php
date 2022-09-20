d4fb5b54453ac1c0e241ca88ccb5bbb71791ab3a
alfiantaufani <alfiantaufani@gmail.com>
2022-09-20 20:38:22 +0700

    asfsaf

--- web/application/controllers/api/PembayaranController.php
+++ web/application/controllers/api/PembayaranController.php
@@ -43,7 +43,7 @@ class PembayaranController extends CI_Controller
                 'quantity'  => 1
             ];
         }
-        echo json_encode($item);
+        echo json_encode($this->input->get('kode'));
 
         // $init->setAmount($this->input->get('total_nominal')); // for close payment
         // $signature = $init->createSignature();
